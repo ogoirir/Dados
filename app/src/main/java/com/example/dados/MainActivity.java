5b87e5b0734a05edef60b9979d4ee8d24dd6f174
ogoirir <50515589+ogoirir@users.noreply.github.com>
2020-11-12 18:44:55 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/dados/MainActivity.java
+++ app/src/main/java/com/example/dados/MainActivity.java
@@ -13,7 +13,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
-
+//prueba
 public class MainActivity extends AppCompatActivity {
     //Creo las variables que guardan el valor del ultimo dado que se genera, el numero de dados y el bonificador
     public int ultDado, bonDados;
